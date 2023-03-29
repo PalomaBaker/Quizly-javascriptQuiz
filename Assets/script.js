5a331fdd320c22424064897d74e26f3728633142
Paloma Baker <paloma.d.baker@gmail.com>
2023-03-29 09:04:16 -0700

    adding game logic and html

--- Assets/script.js
+++ Assets/script.js
@@ -1,39 +1,92 @@
-var startBtn = document.getElementById('#start');
-
-// Get the start button element from the DOM
-const startButton = document.querySelector('#start');
-
-// Add an event listener to the start button
-startButton.addEventListener('click', startQuiz);
-
-// Define the startQuiz function
-function startQuiz() {
-  // Start the timer by setting an interval to decrement a timer variable every second
-  let timeLeft = 60; // Starting time in seconds
-  const timer = setInterval(function() {
-    timeLeft--;
-    // Update the timer display
-    document.querySelector('#timer').textContent = timeLeft;
-
-    // End the quiz if time runs out
-    if (timeLeft <= 0) {
-      clearInterval(timer);
-      endQuiz();
-    }
-  }, 1000);
-
-  // Display the first quiz question
-  const quizContainer = document.querySelector('#quiz-container');
-  const question = document.createElement('h2');
-  question.textContent = quizQuestions[0].question;
-  quizContainer.appendChild(question);
-
-  // Display the answer choices for the first question
-  const answerChoices = document.createElement('ul');
-  for (let i = 0; i < quizQuestions[0].answers.length; i++) {
-    const answer = document.createElement('li');
-    answer.textContent = quizQuestions[0].answers[i];
-    answerChoices.appendChild(answer);
-  }
-  quizContainer.appendChild(answerChoices);
-}
+  // Set the quiz duration and time penalty for wrong answers
+ const quizDuration = 60; // seconds
+ const timePenalty = 10; // seconds
+ 
+ // Select the HTML elements
+ const startButton = document.getElementById("start-button");
+ const quizContainer = document.getElementById("quiz-container");
+ const questionElement = document.getElementById("question");
+ const choicesContainer = document.getElementById("choices-container");
+ const timerElement = document.getElementById("timer");
+ const gameOverContainer = document.getElementById("game-over-container");
+ const initialsInput = document.getElementById("initials-input");
+ const saveButton = document.getElementById("save-button");
+ 
+ // Set the initial values
+ let questionIndex = 0;
+ let timeRemaining = quizDuration;
+ let timerId = null;
+ 
+ // Helper function to start the timer and update the timer element
+ function startTimer() {
+   timerId = setInterval(() => {
+     timeRemaining--;
+     timerElement.textContent = `Time: ${timeRemaining}`;
+ 
+     if (timeRemaining <= 0) {
+       clearInterval(timerId);
+       endGame();
+     }
+   }, 1000);
+ }
+ 
+ // Helper function to display the current question and choices
+ function displayQuestion() {
+   const currentQuestion = quizQuestions[questionIndex];
+ 
+   questionElement.textContent = currentQuestion.question;
+   choicesContainer.innerHTML = "";
+ 
+   currentQuestion.choices.forEach(choice => {
+     const choiceButton = document.createElement("button");
+     choiceButton.textContent = choice;
+     choiceButton.addEventListener("click", () => {
+       if (choice === currentQuestion.answer) {
+         questionIndex++;
+ 
+         if (questionIndex >= quizQuestions.length) {
+           endGame();
+         } else {
+           displayQuestion();
+         }
+       } else {
+         timeRemaining -= timePenalty;
+ 
+         if (timeRemaining <= 0) {
+           clearInterval(timerId);
+           endGame();
+         } else {
+           displayQuestion();
+         }
+       }
+     });
+ 
+     choicesContainer.appendChild(choiceButton);
+   });
+ }
+ 
+ // Helper function to end the game and show the game over screen
+ function endGame() {
+   clearInterval(timerId);
+ 
+   quizContainer.style.display = "none";
+   gameOverContainer.style.display = "block";
+ }
+ 
+ // Event listener for the start button
+ startButton.addEventListener("click", () => {
+   startButton.style.display = "none";
+   quizContainer.style.display = "block";
+ 
+   startTimer();
+   displayQuestion();
+ });
+ 
+ // Event listener for the save button
+ saveButton.addEventListener("click", () => {
+   const initials = initialsInput.value;
+   const score = timeRemaining;
+ 
+   // Save the initials and score to a database or localStorage
+ });
+ 
